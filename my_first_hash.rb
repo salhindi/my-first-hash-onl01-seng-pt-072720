10f0ce6c72a9e889870104c752bea9b38d95345c
Sara A <calisaraalhindi@gmail.com>
2020-08-08 20:04:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
-end
+  sara = {"age" => 24,
+  "height" => 5}
+end 
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice
 
 
 def shipping_manifest
