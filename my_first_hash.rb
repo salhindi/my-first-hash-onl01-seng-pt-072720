611c38a6a6a998a5e5efafdd7dbe63f684fa6686
Sara A <calisaraalhindi@gmail.com>
2020-08-08 20:12:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,8 +23,7 @@ def retrieval
                   "oil paintings" => 3
                  }
                end
-             end
-
+             
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 
@@ -38,7 +37,7 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets" => 2, "gun powder" => 4]
 end 
-end
+
 
   # add 4 gun powder to the shipping_manifest hash below
 
