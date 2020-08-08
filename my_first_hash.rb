aa4ba106eb0f7e382c6e467fdf1194f6e518d259
Sara A <calisaraalhindi@gmail.com>
2020-08-08 20:10:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,12 +35,13 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets" => 2, "gun powder" => 4]
+end 
+return shipping_manifest
+end
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
 
   # return the shipping_manifest hash below
-return shipping_manifest
-end
-end
+
