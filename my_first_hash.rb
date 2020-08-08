bab95045b9598394aeb408017876ab498e8bd913
Sara A <calisaraalhindi@gmail.com>
2020-08-08 20:27:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,8 +37,9 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-                 shipping_manifest["muskets"]
-end  
+                 shipping_manifest["muskets"] = 2
+                 shipping_manifest["gun powder"] = 4
+                 end  
   # add 2 muskets to the shipping_manifest hash below
 
 
