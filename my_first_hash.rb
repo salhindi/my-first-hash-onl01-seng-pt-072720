9e09412058137cc153f684b587be2e5756cbe9fe
Sara A <calisaraalhindi@gmail.com>
2020-08-08 20:14:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,6 +13,7 @@ def shipping_manifest
                  }
                  return the_manifest("oil paintings")
 end
+end
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
